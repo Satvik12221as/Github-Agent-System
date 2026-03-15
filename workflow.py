4c9252148be6558f97cc8b73da7b90376f7b7595
Satvik12221as <sanu8800971634@gmail.com>
2026-03-16 00:08:07 +0530

    Feat: PR opener agent with Github interaction

--- workflow.py
+++ workflow.py
@@ -6,7 +6,7 @@ from agents.code_reader import code_reader_agent
 from agents.planner import planner_agent
 from agents.code_writer import code_writer_agent
 from agents.test_writer import run_test_writer as test_writer_agent
-from agents.placeholder import pr_opener_agent
+from agents.pr_opener import open_pull_request_agent as pr_opener_agent
 from utils.logger import get_logger
 
 load_dotenv()
